9346ca35226914f61b977c1a3f3164aca3d68517
Roy Chiu <rychiu13@gmail.com>
2018-04-20 23:33:59 -0500

    Initial commit, webcam setup and grayscale conversion

---
+++ of-hand-tracking/src/ofApp.cpp
@@ -0,0 +1,85 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    webcam_feed.initGrabber(320, 240); // Use default capture device, window size set to 320x240 px
+    
+    // Set sizes of each image that correspond to video feed - prevent compression errors and other bugs
+    color_frame.allocate(320, 240);
+    grayscale_frame.allocate(320, 240);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    // Update image with next frame/image (how videos are produced)
+    webcam_feed.update();
+    // Check that there is a new frame to load - reduces amount of work by constant updates
+    // If there is a new frame, update color and grayscaled images to continue producing a video
+    if (webcam_feed.isFrameNew()){
+        color_frame.setFromPixels(webcam_feed.getPixels());
+        grayscale_frame = color_frame;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    // Display color frames/feeds on screen with one corner at (x, y)
+    color_frame.draw(10, 10);
+    grayscale_frame.draw(350, 10);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    // Keyboard shortcuts to help with hand tracking methods
+}
+
+////--------------------------------------------------------------
+//void ofApp::keyReleased(int key){
+//
+//}
+//
+////--------------------------------------------------------------
+//void ofApp::mouseMoved(int x, int y ){
+//
+//}
+//
+////--------------------------------------------------------------
+//void ofApp::mouseDragged(int x, int y, int button){
+//
+//}
+//
+////--------------------------------------------------------------
+//void ofApp::mousePressed(int x, int y, int button){
+//
+//}
+//
+////--------------------------------------------------------------
+//void ofApp::mouseReleased(int x, int y, int button){
+//
+//}
+//
+////--------------------------------------------------------------
+//void ofApp::mouseEntered(int x, int y){
+//
+//}
+//
+////--------------------------------------------------------------
+//void ofApp::mouseExited(int x, int y){
+//
+//}
+//
+////--------------------------------------------------------------
+//void ofApp::windowResized(int w, int h){
+//
+//}
+//
+////--------------------------------------------------------------
+//void ofApp::gotMessage(ofMessage msg){
+//
+//}
+//
+////--------------------------------------------------------------
+//void ofApp::dragEvent(ofDragInfo dragInfo){
+//
+//}
+
---
+++ of-hand-tracking/src/ofApp.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+
+class ofApp : public ofBaseApp{
+    ofVideoGrabber webcam_feed; // Use laptop webcam to get feed of pixels and updates
+    
+    ofxCvColorImage color_frame; // webcam frame with 3 channel colors
+    ofxCvGrayscaleImage grayscale_frame; // webcam frame with colors removed - convert to monochrome frame
+    
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+//        void keyReleased(int key);
+//        void mouseMoved(int x, int y );
+//        void mouseDragged(int x, int y, int button);
+//        void mousePressed(int x, int y, int button);
+//        void mouseReleased(int x, int y, int button);
+//        void mouseEntered(int x, int y);
+//        void mouseExited(int x, int y);
+//        void windowResized(int w, int h);
+//        void dragEvent(ofDragInfo dragInfo);
+//        void gotMessage(ofMessage msg);
+		
+};
